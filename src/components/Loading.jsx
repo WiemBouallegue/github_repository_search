39ab9564a148e10f584262e3995386673ad6a25b
Wiem-Bouallegue <wiem.bouallegue@kyndryl.com>
2023-06-05 19:40:09 +0200

    create loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+export const Loading = () => {
+  return (
+    <div role="status" class="max-w-sm mx-auto animate-pulse ">
+      <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 sm:w-64 md:w-80 lg:w-96 xl:w-120 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 sm:w-64 md:w-80 lg:w-96 xl:w-120 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 sm:w-64 md:w-80 lg:w-96 xl:w-120 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 sm:w-64 md:w-80 lg:w-96 xl:w-120 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 sm:w-64 md:w-80 lg:w-96 xl:w-120 mb-2.5"></div>
+      <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 sm:w-64 md:w-80 lg:w-96 xl:w-120 mb-2.5"></div>
+      <span class="sr-only">Loading...</span>
+    </div>
+  );
+};
